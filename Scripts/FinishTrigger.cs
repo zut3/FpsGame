5ab6187765f082ccdae1fa1d782fc6ddbf287584
zut3 <z.gl3b@yandex.ru>
2022-02-11 18:09:28 +0300

    ver 0.5.4

--- Scripts/FinishTrigger.cs
+++ Scripts/FinishTrigger.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class FinishTrigger : MonoBehaviour
 {
     [SerializeField] private InteractofPlatform _interact;
+    [SerializeField] private UnityEvent _easterevent;
+    
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Win!");
         _interact.ExitfromPlatform();
+        _easterevent.Invoke();
     }
 }
